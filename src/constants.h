381660df692a8aee0e74967bc35fbeb7c342bed6
Itay <Itay-kun@users.noreply.github.com>
2019-08-04 14:43:50 +0300

    Update constants.h
    updated some of the constants to 2019 project

--- src/constants.h
+++ src/constants.h
@@ -1,34 +1,32 @@
 /*
  * constants.h
  *
- *  Created on: Jan 18, 2016
+ *  Created on: august, 2019
  */
 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
 /* Size constants */
-#define MAX_MEMORY_SIZE 1000
+#define MAX_MEMORY_SIZE 4096 //UPDATED
 #define MAX_FILE_ZISE 1000
-#define MEMORY_START 100
-#define LINE_MAX_LEN 81 /* Extra one for \0 */
+#define MEMORY_START 100 //UPDATED
+#define LINE_MAX_LEN 81 /* Extra one for \0 */ //UPDATED
 #define MAX_ARG_COUNT ((LINE_MAX_LEN - 3)/2 + 1) /* Max arg count assuming shortest command is 3 letters and shortest args are at least 1 character */
-#define DECODE_SIZE 2
-#define OPERAND_SIZE 2
-#define RED_OPERAND_SIZE 6
-#define OPCODE_SIZE 4
-#define GROUP_SIZE 2
-#define RND_SIZE 2
-#define UNUSED_SIZE 1
-#define VALUE_SIZE 13
-#define OPCODES_AMOUNT 16
-#define WORD_SIZE 15
-#define REG_AMOUNT 8
+#define DECODE_SIZE 2 //UPDATED
+#define OPERAND_SIZE 2 //UPDATED
+#define RED_OPERAND_SIZE 6 /*what is red operand?*/
+#define OPCODE_SIZE 4 //UPDATED
+#define UNUSED_SIZE 4 //UPDATED
+#define VALUE_SIZE 13 /*What Is It?*/
+#define OPCODES_AMOUNT 16 //UPDATED
+#define WORD_SIZE 14 //UPDATED
+#define REG_AMOUNT 8 //UPDATED
 #define EXT_LEN 4 /* Length of the extension file name. */
-#define INPUT_EXT ".as"
-#define OBJ_EXT ".ob"
-#define EXT_EXT ".ext"
-#define ENT_EXT ".ent"
+#define INPUT_EXT ".as" //UPDATED
+#define OBJ_EXT ".ob" //UPDATED
+#define EXT_EXT ".ext" //UPDATED
+#define ENT_EXT ".ent" //UPDATED
 
 /*
  *	The three types of encode:
@@ -36,7 +34,8 @@
  */
 enum ENCODE {
 	ABS = 0, EXT, RLC
-};
+}; //UPDATED
+
 /*
  * The four types of addressing:
  * immediate, direct, random and direct-register.
@@ -47,36 +46,36 @@ typedef enum ADDRESSING {
 /* Arguments amount enums */
 enum NUM_OF_ARGS {
 	NO_ARGS = 0, ONE_ARG, TWO_ARGS
-};
+}; //UPDATED
 
 /* Registers enum table */
 typedef enum {
-	R0, R1, R2, R3, R4, R5, R6, R7
-} reg;
+	r0, r1, r2, r3, r4, r5, r6, r7
+} reg; //UPDATED
 
 /* Opcodes enum table */
 typedef enum {
-	MOV,
-	CMP,
-	ADD,
-	SUB,
-	NOT,
-	CLR,
-	LEA,
-	INC,
-	DEC,
-	JMP,
-	BNE,
-	RED,
-	PRN,
-	JSR,
-	RTS,
-	STOP
-} OPCODE; /* !Retain the current order! see info in inst_type */
+	mov,
+	cmp,
+	add,
+	sub,
+	not,
+	clr,
+	lea,
+	inc,
+	dec,
+	jmp,
+	bne,
+	red,
+	prt,
+	jsr,
+	rts,
+	stop
+} OPCODE; /* !Retain the current order! see info in inst_type */ //UPDATED
 
 typedef enum {
-	EMPTY, COMMENT, INSTUCTION, ACTION
-} sentence_type;
+	EMPTY, COMMENT, INSTUCTION, ACTION, MACRO
+} sentence_type; //UPDATED //Added 1 more sentance type
 typedef enum {
 	DOT_DATA = OPCODES_AMOUNT, DOT_STRING, DOT_EXTERN, DOT_ENTRY
 } inst_type; /* The order this enum and the OPCODE must stay identical to the order of the "ops" array in the getLine function so the index will match the enums values*/
@@ -95,7 +94,7 @@ typedef enum {
 /* Flag status */
 typedef enum {
 	ASM_SUCCESS, ASM_FAILURE
-} FLAG;
+} FLAG; /*if i'm correct, this is PSW in our code*/
 
 typedef enum {
 	false = 0, true = 1
